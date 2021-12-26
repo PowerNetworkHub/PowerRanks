6ac4917ba1e52535ef89d50c9bc8deb47ab16d56
Sven Arends <github@svenar.nl>
2021-12-26 21:46:07 +0100

    Added weight to rank list

--- src/main/java/nl/svenar/PowerRanks/Commands/rank/cmd_listranks.java
+++ src/main/java/nl/svenar/PowerRanks/Commands/rank/cmd_listranks.java
@@ -1,6 +1,8 @@
 package nl.svenar.PowerRanks.Commands.rank;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -11,6 +13,7 @@ import nl.svenar.PowerRanks.Commands.PowerCommand;
 import nl.svenar.PowerRanks.Data.Messages;
 import nl.svenar.PowerRanks.Data.Users;
 import nl.svenar.common.structure.PRRank;
+import nl.svenar.common.utils.PRUtil;
 
 public class cmd_listranks extends PowerCommand {
 
@@ -24,16 +27,25 @@ public class cmd_listranks extends PowerCommand {
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 		if (sender.hasPermission("powerranks.cmd.listranks")) {
-			ArrayList<PRRank> ranks = users.getGroups();
-			sender.sendMessage(ChatColor.BLUE + "===" + ChatColor.DARK_AQUA + "----------" + ChatColor.AQUA + plugin.getDescription().getName() + ChatColor.DARK_AQUA + "----------" + ChatColor.BLUE + "===");
-			// sender.sendMessage(ChatColor.DARK_GREEN + "Number of ranks: " + ChatColor.GREEN + ranks.size());
+			List<PRRank> ranks = users.getGroups();
+			sender.sendMessage(ChatColor.BLUE + "===" + ChatColor.DARK_AQUA + "----------" + ChatColor.AQUA
+					+ plugin.getDescription().getName() + ChatColor.DARK_AQUA + "----------" + ChatColor.BLUE + "===");
+			// sender.sendMessage(ChatColor.DARK_GREEN + "Number of ranks: " +
+			// ChatColor.GREEN + ranks.size());
 			sender.sendMessage(ChatColor.AQUA + "Ranks (" + ranks.size() + "):");
 			int index = 0;
+
+			ranks = new ArrayList<>(new HashSet<>(ranks));
+			ranks = PRUtil.reverseRanks(PRUtil.sortRanksByWeight(ranks));
+
 			for (PRRank rank : ranks) {
 				index++;
-				sender.sendMessage(ChatColor.DARK_GREEN + "#" + index + ". " + ChatColor.GREEN + rank.getName() + ChatColor.RESET + " " + PowerRanks.chatColor(rank.getPrefix(), true));
+				sender.sendMessage(ChatColor.DARK_GREEN + "#" + index + ". " + ChatColor.GRAY + "(" + rank.getWeight()
+						+ ")" + ChatColor.GREEN + rank.getName() + ChatColor.RESET + " "
+						+ PowerRanks.chatColor(rank.getPrefix(), true));
 			}
-			sender.sendMessage(ChatColor.BLUE + "===" + ChatColor.DARK_AQUA + "------------------------------" + ChatColor.BLUE + "===");
+			sender.sendMessage(ChatColor.BLUE + "===" + ChatColor.DARK_AQUA + "------------------------------"
+					+ ChatColor.BLUE + "===");
 		} else {
 			Messages.noPermission(sender);
 		}
