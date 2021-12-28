a4a212edd59860a7652b4df47e51122dc98e2088
Sven Arends <github@svenar.nl>
2021-12-28 22:19:00 +0100

    Move experimenting with tab

---
+++ src/main/java/nl/svenar/PowerRanks/External/TABHook.java
@@ -0,0 +1,141 @@
+package nl.svenar.PowerRanks.External;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.scheduler.BukkitRunnable;
+
+import me.neznamy.tab.api.TabAPI;
+import nl.svenar.PowerRanks.PowerRanks;
+import nl.svenar.PowerRanks.Cache.CacheManager;
+import nl.svenar.PowerRanks.Util.PluginReloader;
+import nl.svenar.common.structure.PRPlayer;
+import nl.svenar.common.structure.PRRank;
+import nl.svenar.common.utils.PRUtil;
+
+public class TABHook {
+
+    private boolean isReady = false;
+    private List<PRRank> sortedRanks = new ArrayList<PRRank>();
+    private PRTABFeature prTabFeature = null;
+
+    public void setup() {
+        prTabFeature = new PRTABFeature("powerranks", "");
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (PowerRanks.getInstance().getServer().getPluginManager().isPluginEnabled("TAB")) {
+                    TabAPI.getInstance().getFeatureManager().registerFeature("powerranks",
+                            new PRTABFeature("powerranks", ""));
+                    load();
+                    this.cancel();
+                }
+            }
+        }.runTaskTimer(PowerRanks.getInstance(), 5, 1);
+
+        new BukkitRunnable() {
+            List<PRRank> ranks = new ArrayList<PRRank>(CacheManager.getRanks());
+
+            @Override
+            public void run() {
+                if (isReady) {
+                    // PowerRanks.getInstance().getLogger().warning(CacheManager.getRanks().size() +
+                    // " - " + ranks.size() + " | " +
+                    // TabAPI.getInstance().getFeatureManager().isFeatureEnabled("powerranks"));
+
+                    if (CacheManager.getRanks().size() != ranks.size()) {
+                        ranks = new ArrayList<PRRank>(CacheManager.getRanks());
+                        // updateRanks();
+                        isReady = false;
+                        load();
+                        // this.cancel();
+                        // setup();
+                    }
+
+                    // if (!TabAPI.getInstance().getFeatureManager().isFeatureEnabled("powerranks"))
+                    // {
+                    // TabAPI.getInstance().getFeatureManager().registerFeature("powerranks", new
+                    // PRTABFeature("powerranks", ""));
+                    // }
+                }
+            }
+        }.runTaskTimer(PowerRanks.getInstance(), 20 * 5, 20 * 5);
+    }
+
+    private void load() {
+        if (isReady) {
+            return;
+        }
+
+        if (!updateRanks()) {
+            isReady = true;
+            // try {
+            // TabAPI.getInstance().getFeatureManager().registerFeature("powerranks", new
+            // PRTABFeature("powerranks", ""));
+            // } catch (Exception e) {}
+            TabAPI.getInstance().debug("Hooked into PowerRanks");
+        }
+    }
+
+    public boolean updateRanks() {
+        sortedRanks = getSortedRanks();
+
+        boolean doRestart = false;
+
+        List<String> sortingList = new ArrayList<String>();
+        for (PRRank rank : sortedRanks) {
+            sortingList.add(rank.getName());
+        }
+
+        List<String> sortingTypes = TabAPI.getInstance().getConfig().getStringList("scoreboard-teams.sorting-types",
+                new ArrayList<String>());
+        List<String> newSortingTypes = new ArrayList<String>();
+
+        newSortingTypes.add("GROUPS:" + String.join(",", sortingList));
+
+        for (String type : sortingTypes) {
+            if (!type.split(":")[0].equals("GROUPS")) {
+                newSortingTypes.add(type);
+            } else {
+
+                // PowerRanks.getInstance().getLogger().warning(type.split(":")[1] + " - " + String.join(",", sortingList));
+
+                if (String.join(",", sortingList).length() != type.split(":")[1].length()) {
+                    // doRestart = true; // Restart the plugin
+                }
+            }
+        }
+        TabAPI.getInstance().getConfig().set("scoreboard-teams.sorting-types", newSortingTypes);
+
+        TabAPI.getInstance().getConfig().save();
+
+        if (doRestart) {
+            TabAPI.getInstance().getFeatureManager().unregisterFeature("powerranks");
+            PluginReloader pluginReloader = new PluginReloader();
+            TabAPI.getInstance().debug("[PR] Restarting TAB...");
+            pluginReloader.reload("TAB");
+            TabAPI.getInstance().debug("[PR] Restarted TAB.");
+            TabAPI.getInstance().getFeatureManager().registerFeature("powerranks", prTabFeature);
+            load();
+        }
+
+        return doRestart;
+
+    }
+
+    private List<PRRank> getSortedRanks() {
+        return PRUtil.reverseRanks(PRUtil.sortRanksByWeight(CacheManager.getRanks()));
+    }
+
+    static List<PRRank> getSortedRanks(PRPlayer prPlayer) {
+        List<PRRank> playerRanks = new ArrayList<PRRank>();
+        for (String rankname : prPlayer.getRanks()) {
+            if (CacheManager.getRank(rankname) != null) {
+                playerRanks.add(CacheManager.getRank(rankname));
+            }
+        }
+
+        return PRUtil.reverseRanks(PRUtil.sortRanksByWeight(playerRanks));
+    }
+}
--- src/main/java/nl/svenar/PowerRanks/PowerRanks.java
+++ src/main/java/nl/svenar/PowerRanks/PowerRanks.java
@@ -60,7 +60,9 @@ import nl.svenar.PowerRanks.Events.OnPreCommand;
 import nl.svenar.PowerRanks.Events.OnSignChanged;
 import nl.svenar.PowerRanks.Events.OnWorldChange;
 import nl.svenar.PowerRanks.External.PowerRanksExpansion;
+import nl.svenar.PowerRanks.External.TABHook;
 import nl.svenar.PowerRanks.External.VaultHook;
+import nl.svenar.PowerRanks.Util.Util;
 import nl.svenar.PowerRanks.addons.AddonsManager;
 import nl.svenar.PowerRanks.api.PowerRanksAPI;
 import nl.svenar.PowerRanks.gui.GUI;
@@ -109,6 +111,7 @@ public class PowerRanks extends JavaPlugin implements Listener {
 	public static boolean vaultEconomyEnabled = false;
 	public static boolean vaultPermissionsEnabled = false;
 	public static PowerRanksExpansion placeholderapiExpansion;
+	public static TABHook plugin_hook_tab;
 	public static boolean plugin_hook_deluxetags = false;
 	public static boolean plugin_hook_nametagedit = false;
 	// Soft Dependencies
@@ -421,6 +424,8 @@ public class PowerRanks extends JavaPlugin implements Listener {
 				&& getConfigBool("plugin_hook.vault_permissions");
 		boolean has_placeholderapi = this.getServer().getPluginManager().getPlugin("PlaceholderAPI") != null
 				&& getConfigBool("plugin_hook.placeholderapi");
+		boolean has_tab = this.getServer().getPluginManager().getPlugin("TAB") != null
+				&& getConfigBool("plugin_hook.tab");
 		boolean has_deluxetags = this.getServer().getPluginManager().getPlugin("DeluxeTags") != null
 				&& getConfigBool("plugin_hook.deluxetags");
 		boolean has_nametagedit = this.getServer().getPluginManager().getPlugin("NametagEdit") != null
@@ -453,6 +458,13 @@ public class PowerRanks extends JavaPlugin implements Listener {
 			PowerRanks.placeholderapiExpansion = null;
 		}
 
+		if (has_tab) {
+			PowerRanks.log.info("TAB found!");
+			PowerRanks.log.warning("TAB INTEGRATION IS EXPERIMENTAL, USE AT YOUR OWN RISK!");
+			plugin_hook_tab = new TABHook();
+			plugin_hook_tab.setup();
+		}
+
 		if (has_deluxetags) {
 			PowerRanks.log.info("DeluxeTags found!");
 			plugin_hook_deluxetags = true;
@@ -464,7 +476,8 @@ public class PowerRanks extends JavaPlugin implements Listener {
 			setup_nte();
 		}
 
-		if (!has_vault_economy && !has_vault_permissions && !has_placeholderapi && !has_deluxetags && !has_nametagedit)
+		if (!has_vault_economy && !has_vault_permissions && !has_placeholderapi && !has_tab && !has_deluxetags
+				&& !has_nametagedit)
 			PowerRanks.log.info("No other plugins found! Working stand-alone.");
 	}
 
@@ -480,6 +493,10 @@ public class PowerRanks extends JavaPlugin implements Listener {
 				}
 			}
 		}.runTaskTimer(this, 20, 20);
+	}
+
+	public TABHook getTABHook() {
+		return plugin_hook_tab;
 	}
 
 	private boolean handle_update_checking() {
