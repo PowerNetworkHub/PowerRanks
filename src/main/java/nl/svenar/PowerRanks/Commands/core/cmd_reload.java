5440266586574ad55dd7f43c5cf56fa1884570fa
Sven Arends <github@svenar.nl>
2021-12-28 22:11:13 +0100

    Moved utils to Util directory & changed reloading behaviour

--- src/main/java/nl/svenar/PowerRanks/Commands/core/cmd_reload.java
+++ src/main/java/nl/svenar/PowerRanks/Commands/core/cmd_reload.java
@@ -2,18 +2,16 @@ package nl.svenar.PowerRanks.Commands.core;
 
 import java.util.ArrayList;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.PluginManager;
 
 import nl.svenar.PowerRanks.PowerRanks;
 import nl.svenar.PowerRanks.Cache.CacheManager;
 // import nl.svenar.PowerRanks.Cache.CachedConfig;
 import nl.svenar.PowerRanks.Commands.PowerCommand;
 import nl.svenar.PowerRanks.Data.Messages;
+import nl.svenar.PowerRanks.Util.PluginReloader;
 
 public class cmd_reload extends PowerCommand {
 
@@ -40,10 +38,12 @@ public class cmd_reload extends PowerCommand {
 				} else if (args[0].equalsIgnoreCase("plugin")) {
 					
 					Messages.messageCommandReloadPlugin(sender);
-					final PluginManager plg = Bukkit.getPluginManager();
-					final Plugin plgname = plg.getPlugin(PowerRanks.pdf.getName());
-					plg.disablePlugin(plgname);
-					plg.enablePlugin(plgname);
+					PluginReloader pluginReloader = new PluginReloader();
+					pluginReloader.reload(PowerRanks.pdf.getName());
+					// final PluginManager plg = Bukkit.getPluginManager();
+					// final Plugin plgname = plg.getPlugin(PowerRanks.pdf.getName());
+					// plg.disablePlugin(plgname);
+					// plg.enablePlugin(plgname);
 					Messages.messageCommandReloadPluginDone(sender);
 				
 				} else if (args[0].equalsIgnoreCase("addons")) {
@@ -56,10 +56,12 @@ public class cmd_reload extends PowerCommand {
 				} else if (args[0].equalsIgnoreCase("all")) {
 					
 					Messages.messageCommandReloadPlugin(sender);
-					final PluginManager plg = Bukkit.getPluginManager();
-					final Plugin plgname = plg.getPlugin(PowerRanks.pdf.getName());
-					plg.disablePlugin(plgname);
-					plg.enablePlugin(plgname);
+					PluginReloader pluginReloader = new PluginReloader();
+					pluginReloader.reload(PowerRanks.pdf.getName());
+					// final PluginManager plg = Bukkit.getPluginManager();
+					// final Plugin plgname = plg.getPlugin(PowerRanks.pdf.getName());
+					// plg.disablePlugin(plgname);
+					// plg.enablePlugin(plgname);
 					Messages.messageCommandReloadPluginDone(sender);
 
 
