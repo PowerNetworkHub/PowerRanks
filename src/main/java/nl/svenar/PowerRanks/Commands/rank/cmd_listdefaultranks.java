99abae8ab1b879c0e8e38c8f5d673f25dd81653b
Sven Arends <github@svenar.nl>
2022-01-14 12:12:28 +0100

    Added command /pr listdefaultranks to list all default ransk that will be granted t onew players

---
+++ src/main/java/nl/svenar/PowerRanks/Commands/rank/cmd_listdefaultranks.java
@@ -0,0 +1,64 @@
+package nl.svenar.PowerRanks.Commands.rank;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import nl.svenar.PowerRanks.PowerRanks;
+import nl.svenar.PowerRanks.Cache.CacheManager;
+import nl.svenar.PowerRanks.Commands.PowerCommand;
+import nl.svenar.PowerRanks.Data.Messages;
+import nl.svenar.common.structure.PRRank;
+import nl.svenar.common.utils.PRUtil;
+
+public class cmd_listdefaultranks extends PowerCommand {
+
+	public cmd_listdefaultranks(PowerRanks plugin, String command_name, COMMAND_EXECUTOR ce) {
+		super(plugin, command_name, ce);
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		if (sender.hasPermission("powerranks.cmd.listdefaultranks")) {
+			if (args.length == 0) {
+				List<PRRank> ranks = CacheManager.getDefaultRanks();
+				sender.sendMessage(ChatColor.BLUE + "===" + ChatColor.DARK_AQUA + "----------" + ChatColor.AQUA
+						+ plugin.getDescription().getName() + ChatColor.DARK_AQUA + "----------" + ChatColor.BLUE
+						+ "===");
+				// sender.sendMessage(ChatColor.DARK_GREEN + "Number of ranks: " +
+				// ChatColor.GREEN + ranks.size());
+				sender.sendMessage(ChatColor.AQUA + "Ranks (" + ranks.size() + "):");
+				int index = 0;
+
+				ranks = new ArrayList<>(new HashSet<>(ranks));
+				ranks = PRUtil.reverseRanks(PRUtil.sortRanksByWeight(ranks));
+
+				for (PRRank rank : ranks) {
+					index++;
+					sender.sendMessage(
+							ChatColor.DARK_GREEN + "#" + index + ". " + ChatColor.GRAY + "(" + rank.getWeight()
+									+ ") " + ChatColor.GREEN + rank.getName() + ChatColor.RESET + " "
+									+ PowerRanks.chatColor(rank.getPrefix(), true));
+				}
+				sender.sendMessage(ChatColor.BLUE + "===" + ChatColor.DARK_AQUA + "------------------------------"
+						+ ChatColor.BLUE + "===");
+			} else {
+				Messages.messageCommandUsageListDefaultRanks(sender);
+			}
+		} else {
+			Messages.noPermission(sender);
+		}
+
+		return false;
+	}
+
+	public ArrayList<String> tabCompleteEvent(CommandSender sender, String[] args) {
+		ArrayList<String> tabcomplete = new ArrayList<String>();
+
+		return tabcomplete;
+	}
+}
