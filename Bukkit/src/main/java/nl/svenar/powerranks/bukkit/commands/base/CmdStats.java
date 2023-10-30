40cee83081848f218fb979f91dfd5d48e03c583d
Sven Arends <github@svenar.nl>
2023-10-30 10:38:37 +0100

    Updated stats command

--- Bukkit/src/main/java/nl/svenar/powerranks/bukkit/commands/base/CmdStats.java
+++ Bukkit/src/main/java/nl/svenar/powerranks/bukkit/commands/base/CmdStats.java
@@ -28,7 +28,7 @@ public class CmdStats extends PowerBaseCommand {
     public CmdStats(PowerRanks plugin) {
         super(plugin);
     }
-    
+
     @Subcommand("stats")
     @CommandPermission("powerranks.cmd.stats")
     public void onStats(CommandSender sender) {
@@ -36,73 +36,82 @@ public class CmdStats extends PowerBaseCommand {
     }
 
     private void messageStats(CommandSender sender) {
-		SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
-		format.setTimeZone(TimeZone.getTimeZone("UTC"));
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+        format.setTimeZone(TimeZone.getTimeZone("UTC"));
 
-		int addonCount = 0;
-		for (Entry<File, Boolean> prAddon : AddonsManager.loadedAddons.entrySet()) {
-			if (prAddon.getValue() == true)
-				addonCount++;
-		}
-		Instant current_time = Instant.now();
+        int addonCount = 0;
+        for (Entry<File, Boolean> prAddon : AddonsManager.loadedAddons.entrySet()) {
+            if (prAddon.getValue() == true)
+                addonCount++;
+        }
+        Instant current_time = Instant.now();
 
-		sender.sendMessage(PowerColor.ChatColor.DARK_AQUA + "--------" + PowerColor.ChatColor.DARK_BLUE + PowerRanks.pdf.getName()
-				+ PowerColor.ChatColor.DARK_AQUA + "--------");
-		sender.sendMessage(PowerColor.ChatColor.GREEN + "Server version: " + PowerColor.ChatColor.DARK_GREEN + Bukkit.getVersion() + " | "
-				+ Bukkit.getServer().getBukkitVersion());
-		sender.sendMessage(
-				PowerColor.ChatColor.GREEN + "Java version: " + PowerColor.ChatColor.DARK_GREEN + System.getProperty("java.version"));
-		sender.sendMessage(PowerColor.ChatColor.GREEN + "Storage method: " + PowerColor.ChatColor.DARK_GREEN
-				+ PowerRanks.getConfigManager().getString("storage.type", "yaml").toUpperCase());
-		sender.sendMessage(PowerColor.ChatColor.GREEN + "Uptime: " + PowerColor.ChatColor.DARK_GREEN
-				+ format.format(Duration.between(PowerRanks.powerranks_start_time, current_time).toMillis()));
-		sender.sendMessage(
-				PowerColor.ChatColor.GREEN + "PowerRanks Version: " + PowerColor.ChatColor.DARK_GREEN + PowerRanks.pdf.getVersion());
-		sender.sendMessage(PowerColor.ChatColor.GREEN + "Registered ranks: " + PowerColor.ChatColor.DARK_GREEN + PRCache.getRanks().size());
-		sender.sendMessage(
-				PowerColor.ChatColor.GREEN + "Registered players: " + PowerColor.ChatColor.DARK_GREEN + PRCache.getPlayers().size());
-		sender.sendMessage(PowerColor.ChatColor.GREEN + "Registered addons: " + PowerColor.ChatColor.DARK_GREEN + addonCount);
+        sendMessage(sender, "list-header");
+        sender.sendMessage(PowerColor.ChatColor.GREEN + "Server version: " + PowerColor.ChatColor.DARK_GREEN
+                + Bukkit.getVersion() + " | "
+                + Bukkit.getServer().getBukkitVersion());
+        sender.sendMessage(
+                PowerColor.ChatColor.GREEN + "Java version: " + PowerColor.ChatColor.DARK_GREEN
+                        + System.getProperty("java.version"));
+        sender.sendMessage(PowerColor.ChatColor.GREEN + "Storage method: " + PowerColor.ChatColor.DARK_GREEN
+                + PowerRanks.getConfigManager().getString("storage.type", "yaml").toUpperCase());
+        sender.sendMessage(PowerColor.ChatColor.GREEN + "Uptime: " + PowerColor.ChatColor.DARK_GREEN
+                + format.format(Duration.between(PowerRanks.powerranks_start_time, current_time).toMillis()));
+        sender.sendMessage(
+                PowerColor.ChatColor.GREEN + "PowerRanks Version: " + PowerColor.ChatColor.DARK_GREEN
+                        + PowerRanks.pdf.getVersion());
+        sender.sendMessage(PowerColor.ChatColor.GREEN + "Registered ranks: " + PowerColor.ChatColor.DARK_GREEN
+                + PRCache.getRanks().size());
+        sender.sendMessage(
+                PowerColor.ChatColor.GREEN + "Registered players: " + PowerColor.ChatColor.DARK_GREEN
+                        + PRCache.getPlayers().size());
+        sender.sendMessage(
+                PowerColor.ChatColor.GREEN + "Registered addons: " + PowerColor.ChatColor.DARK_GREEN + addonCount);
 
-		boolean hex_color_supported = false;
-		try {
-			"#FF0000a".replace("#FF0000", net.md_5.bungee.api.ChatColor.of("#FF0000") + "");
-			hex_color_supported = true;
-		} catch (Exception | NoSuchMethodError e) {
-			hex_color_supported = false;
-		}
-		sender.sendMessage(PowerColor.ChatColor.GREEN + "RGB colors: "
-				+ (hex_color_supported ? PowerColor.ChatColor.DARK_GREEN + "" : PowerColor.ChatColor.DARK_RED + "un") + "supported");
-		sender.sendMessage(PowerColor.ChatColor.GREEN + "Bungeecord: "
-				+ (PowerRanks.getInstance().getBungeecordManager().isReady() ? PowerColor.ChatColor.DARK_GREEN + "enabled"
-						: PowerColor.ChatColor.DARK_RED + "disabled"));
-		sender.sendMessage(PowerColor.ChatColor.GREEN + "- Connected servers: "
-				+ PowerColor.ChatColor.DARK_GREEN + PowerRanks.getInstance().getBungeecordManager().getServerCount());
+        boolean hex_color_supported = false;
+        try {
+            "#FF0000a".replace("#FF0000", net.md_5.bungee.api.ChatColor.of("#FF0000") + "");
+            hex_color_supported = true;
+        } catch (Exception | NoSuchMethodError e) {
+            hex_color_supported = false;
+        }
+        sender.sendMessage(PowerColor.ChatColor.GREEN + "RGB colors: "
+                + (hex_color_supported ? PowerColor.ChatColor.DARK_GREEN + "" : PowerColor.ChatColor.DARK_RED + "un")
+                + "supported");
+        sender.sendMessage(PowerColor.ChatColor.GREEN + "Bungeecord: "
+                + (PowerRanks.getInstance().getBungeecordManager().isReady()
+                        ? PowerColor.ChatColor.DARK_GREEN + "enabled"
+                        : PowerColor.ChatColor.DARK_RED + "disabled"));
+        sender.sendMessage(PowerColor.ChatColor.GREEN + "- Connected servers: "
+                + PowerColor.ChatColor.DARK_GREEN + PowerRanks.getInstance().getBungeecordManager().getServerCount());
 
-		sender.sendMessage(PowerColor.ChatColor.GREEN + "Plugin hooks:");
-		sender.sendMessage(PowerColor.ChatColor.GREEN + "- Vault Economy: "
-				+ (PowerRanks.vaultEconomyEnabled ? PowerColor.ChatColor.DARK_GREEN + "enabled"
-						: PowerColor.ChatColor.DARK_RED + "disabled"));
-		sender.sendMessage(PowerColor.ChatColor.GREEN + "- Vault Permissions: "
-				+ (PowerRanks.vaultPermissionsEnabled ? PowerColor.ChatColor.DARK_GREEN + "enabled"
-						: PowerColor.ChatColor.DARK_RED + "disabled"));
-		sender.sendMessage(PowerColor.ChatColor.GREEN + "- PlaceholderAPI: "
-				+ (PowerRanks.getPlaceholderapiExpansion() != null ? PowerColor.ChatColor.DARK_GREEN + "enabled"
-						: PowerColor.ChatColor.DARK_RED + "disabled"));
-		sender.sendMessage(PowerColor.ChatColor.GREEN + "- DeluxeTags: "
-				+ (PowerRanks.plugin_hook_deluxetags ? PowerColor.ChatColor.DARK_GREEN + "enabled"
-						: PowerColor.ChatColor.DARK_RED + "disabled"));
-		sender.sendMessage(PowerColor.ChatColor.GREEN + "- NametagEdit: "
-				+ (PowerRanks.plugin_hook_nametagedit ? PowerColor.ChatColor.DARK_GREEN + "enabled"
-						: PowerColor.ChatColor.DARK_RED + "disabled"));
+        sender.sendMessage(PowerColor.ChatColor.GREEN + "Plugin hooks:");
+        sender.sendMessage(PowerColor.ChatColor.GREEN + "- Vault Economy: "
+                + (PowerRanks.vaultEconomyEnabled ? PowerColor.ChatColor.DARK_GREEN + "enabled"
+                        : PowerColor.ChatColor.DARK_RED + "disabled"));
+        sender.sendMessage(PowerColor.ChatColor.GREEN + "- Vault Permissions: "
+                + (PowerRanks.vaultPermissionsEnabled ? PowerColor.ChatColor.DARK_GREEN + "enabled"
+                        : PowerColor.ChatColor.DARK_RED + "disabled"));
+        sender.sendMessage(PowerColor.ChatColor.GREEN + "- PlaceholderAPI: "
+                + (PowerRanks.getPlaceholderapiExpansion() != null ? PowerColor.ChatColor.DARK_GREEN + "enabled"
+                        : PowerColor.ChatColor.DARK_RED + "disabled"));
+        sender.sendMessage(PowerColor.ChatColor.GREEN + "- DeluxeTags: "
+                + (PowerRanks.plugin_hook_deluxetags ? PowerColor.ChatColor.DARK_GREEN + "enabled"
+                        : PowerColor.ChatColor.DARK_RED + "disabled"));
+        sender.sendMessage(PowerColor.ChatColor.GREEN + "- NametagEdit: "
+                + (PowerRanks.plugin_hook_nametagedit ? PowerColor.ChatColor.DARK_GREEN + "enabled"
+                        : PowerColor.ChatColor.DARK_RED + "disabled"));
 
-		Plugin[] plugins = Bukkit.getPluginManager().getPlugins();
-		String pluginNames = "";
-		for (Plugin plugin : plugins) {
-			pluginNames += plugin.getName() + "(" + plugin.getDescription().getVersion() + "), ";
-		}
-		pluginNames = pluginNames.substring(0, pluginNames.length() - 2);
+        Plugin[] plugins = Bukkit.getPluginManager().getPlugins();
+        String pluginNames = "";
+        for (Plugin plugin : plugins) {
+            pluginNames += plugin.getName() + "(" + plugin.getDescription().getVersion() + "), ";
+        }
+        pluginNames = pluginNames.substring(0, pluginNames.length() - 2);
 
-		sender.sendMessage(PowerColor.ChatColor.GREEN + "Plugins (" + plugins.length + "): " + PowerColor.ChatColor.DARK_GREEN + pluginNames);
-		sender.sendMessage(PowerColor.ChatColor.DARK_AQUA + "--------------------------");
-	}
+        sender.sendMessage(PowerColor.ChatColor.GREEN + "Plugins (" + plugins.length + "): "
+                + PowerColor.ChatColor.DARK_GREEN + pluginNames);
+
+        sendMessage(sender, "list-footer");
+    }
 }
